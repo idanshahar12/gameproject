b0ada13d355a9a615990f2525da25dbd37ceae47
Idan <idank162@gmail.com>
2026-01-12 19:28:24 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      
+      // הגדרה עבור כל בקשה שמתחילה ב-/api
       '/api': {
+        // הכתובת של שרת ה-API שלך
         target: 'https://www.freetogame.com/api',
+        // שינוי המקור (origin) של הבקשה לכתובת היעד
         changeOrigin: true,
+        // אופציונלי: מחיקת ה-/api מתחילת ה-URL לפני שליחתו לשרת
+        // לדוגמה, /api/users יהפוך ל- /users
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
